1581f59b44a7e65d413876f6a4c9a274ea7a6332
evilraptor <43874288+evilraptor@users.noreply.github.com>
2023-01-17 17:55:58 +0700

    Update Simulator.cpp

--- prisoner's dilemma/Simulator.cpp
+++ prisoner's dilemma/Simulator.cpp
@@ -1,6 +1,6 @@
 #include "DilemmaHeader.h"
 
-Score Simulator(int game_steps_number, short game_type, int player1_place, int player2_place, int player3_place, Score score, std::vector<std::vector<char>> roundschoices, std::map<int, IStrategy*> players) {
+Score Simulator(int pay_off_matrix[8][6], int game_steps_number, short game_type, int player1_place, int player2_place, int player3_place, Score score, std::vector<std::vector<char>> roundschoices, std::map<int, IStrategy*> players) {
 	if (game_type != 2)std::cin.get();
 	std::vector<int> _score(3);
 	for (int i = 0; i < game_steps_number; i++) {
@@ -14,9 +14,9 @@ Score Simulator(int game_steps_number, short game_type, int player1_place, int p
 		if (game_type == 1) {
 			std::cout << "\n";
 		}
-		RoundResult roundresult(simulationroundchoices);
+		RoundResult roundresult(simulationroundchoices, pay_off_matrix);
 		//score.ChangeScore(roundresult.a, strategy1p, roundresult.b, strategy2p, roundresult.c, strategy3p);
-		score.ChangeRoundScore(roundresult.player1_pay_off,roundresult.player2_pay_off,roundresult.player3_pay_off);
+		score.ChangeRoundScore(roundresult.player1_pay_off, roundresult.player2_pay_off, roundresult.player3_pay_off);
 		/*_score = */score.ShowGameScore(player1_place, player2_place, player3_place, game_type);
 		roundschoices.push_back(simulationroundchoices);
 		if (game_type != 2)std::cout << "do something...\n";
