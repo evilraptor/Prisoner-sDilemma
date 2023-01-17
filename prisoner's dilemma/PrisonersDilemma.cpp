63d2356a9b0ccd9a72a2443a3e5d6ac5dbc50a85
evilraptor <43874288+evilraptor@users.noreply.github.com>
2023-01-17 13:10:49 +0700

    Update PrisonersDilemma.cpp

--- prisoner's dilemma/PrisonersDilemma.cpp
+++ prisoner's dilemma/PrisonersDilemma.cpp
@@ -1,67 +1,67 @@
 // prisoner's dilemma.cpp 
 #include "DilemmaHeader.h" 
 
-class AbstractCreator
-{
-public:
-    //virtual fooCreator() {} 
-    virtual IStrategy* create() const = 0;
-    //virtual ~abstractFooCreator();
-};
-
-template <class C>
-class StrategyCreator : public AbstractCreator
-{
-public:
-    virtual IStrategy* create() const { return new C(); }
-};
-
-class StrategyFactory
-{
-protected:
-    typedef std::map<int, AbstractCreator*> FactoryMap;
-    FactoryMap _factory;
-
-public: 
-    //FooFactory(); 
-    //~virtual FooFactory(); 
-
-    template <class C>
-    void add(const int& id)
-    {
-        typename FactoryMap::iterator it = _factory.find(id);
-        if (it == _factory.end())
-            _factory[id] = new StrategyCreator<C>();
-    }
-
-    IStrategy* create(const int& id)
-    {
-        typename FactoryMap::iterator it = _factory.find(id);
-        if (it != _factory.end())
-            return it->second->create();
-        return 0;
-    }
-    ~StrategyFactory() {
-
-    };
-};
+//class AbstractCreator
+//{
+//public:
+//    //virtual fooCreator() {} 
+//    virtual IStrategy* create() const = 0;
+//    //virtual ~abstractFooCreator();
+//};
+//
+//template <class C>
+//class StrategyCreator : public AbstractCreator
+//{
+//public:
+//    virtual IStrategy* create() const { return new C(); }
+//};
+//
+//class StrategyFactory
+//{
+//protected:
+//    typedef std::map<int, AbstractCreator*> FactoryMap;
+//    FactoryMap _factory;
+//
+//public:
+//    //FooFactory(); 
+//    //~virtual FooFactory(); 
+//
+//    template <class C>
+//    void add(const int& id)
+//    {
+//        typename FactoryMap::iterator it = _factory.find(id);
+//        if (it == _factory.end())
+//            _factory[id] = new StrategyCreator<C>();
+//    }
+//
+//    IStrategy* create(const int& id)
+//    {
+//        typename FactoryMap::iterator it = _factory.find(id);
+//        if (it != _factory.end())
+//            return it->second->create();
+//        return 0;
+//    }
+//    ~StrategyFactory() {
+//
+//    };
+//};
 //////////////////////////////////// 
 int main(int argc, char* argv[])
 {
     StrategyFactory factory;
-    factory.add<StrategyRand>(1);
-    factory.add<StrategyAlwaysC>(2);
-    factory.add<StrategyAlwaysD>(3);
-    factory.add<StrategyWinRound>(4);
-    factory.add<StrategyTeamplay>(5);
-    factory.add<StrategyRevengeful>(6);
+    //factory.add<StrategyRand>(1);
+    //factory.add<StrategyAlwaysC>(2);
+    //factory.add<StrategyAlwaysD>(3);
+    //factory.add<StrategyWinRound>(4);
+    //factory.add<StrategyTeamplay>(5);
+    //factory.add<StrategyRevengeful>(6);
     int playerscount = 0;
     short game_type = 0;
     typedef std::map<int, IStrategy*> Players;//1ое число номер игрока 2ое номер стратегии 
     Players players;//стратегии 
     int game_steps_number;
     if (argc == 1) {
-        std::cout << "choise players and game mode (D-detailed, F-fast, T-tournament, E-to exit)\n";
+        std::cout << "choice players and game mode (D-detailed, F-fast, T-tournament, E-to exit)\n";
         while (game_type == 0) {
             char tmp[256];
             std::cin >> tmp;
@@ -89,9 +89,39 @@ int main(int argc, char* argv[])
         }
         std::cout << "steps count: ";
         std::cin >> game_steps_number;
+
+        Score score;
+        std::vector<std::vector<char>> roundschoices(game_steps_number);//вектор векторов ходов 
+        if (game_type == 3) {//это с турниром 
+            int* players_of_this_rounde = new int[playerscount];
+            for (int i = 0; i < playerscount; i++)players_of_this_rounde[i] = i + 1;
+            int players_count_in_single_game = 3;
+            //Print(a, m); 
+            for (int i = 0; i < players_count_in_single_game; i++) std::cout << players_of_this_rounde[i] << " ";
+            std::cout << "\n";
+            score = Simulator(game_steps_number, game_type, players_of_this_rounde[0], players_of_this_rounde[1], players_of_this_rounde[2], score, roundschoices, players);
+            if (playerscount >= players_count_in_single_game)
+            {
+                while (PlayersOfNewRounde(players_of_this_rounde, playerscount, players_count_in_single_game)) {
+                    for (int i = 0; i < players_count_in_single_game; i++) std::cout << players_of_this_rounde[i] << " ";
+                    std::cout << "\n";
+                    score = Simulator(game_steps_number, game_type, players_of_this_rounde[0], players_of_this_rounde[1], players_of_this_rounde[2], score, roundschoices, players);
+                    //score.MakeItZero();
+                }
+            }
+            score.ShowBestGlobalScore();
+            return 0;
+        }
+        else if ((game_type == 1) || (game_type == 2)) {//ну и без 
+            std::cout << "do something\n";
+            score = Simulator(game_steps_number, game_type, 0, 1, 2, score, roundschoices, players);
+            score.ShowBestGlobalScore();
+            return 0;
+        }
+        return -1;
     }
     else {
-        for (int i = 1;i < argc - 1;i++) {
+        for (int i = 1; i < argc - 1; i++) {
             char tmp = *argv[i];
             //tmp = char(tmp);
             //std::cout << i;
@@ -118,10 +148,10 @@ int main(int argc, char* argv[])
                 playerscount++;
             }
         }
-        game_steps_number = atoi(argv[argc-1]);
+        game_steps_number = atoi(argv[argc - 1]);
         //}
         Score score;
-        std::vector<std::vector<char>> roundschoises(game_steps_number);//вектор векторов ходов 
+        std::vector<std::vector<char>> roundschoices(game_steps_number);//вектор векторов ходов 
         if (game_type == 3) {//это с турниром 
             int* players_of_this_rounde = new int[playerscount];
             for (int i = 0; i < playerscount; i++)players_of_this_rounde[i] = i + 1;
@@ -129,13 +159,13 @@ int main(int argc, char* argv[])
             //Print(a, m); 
             for (int i = 0; i < players_count_in_single_game; i++) std::cout << players_of_this_rounde[i] << " ";
             std::cout << "\n";
-            score = Simulator(game_steps_number, game_type, players_of_this_rounde[0], players_of_this_rounde[1], players_of_this_rounde[2], score, roundschoises, players);
+            score = Simulator(game_steps_number, game_type, players_of_this_rounde[0], players_of_this_rounde[1], players_of_this_rounde[2], score, roundschoices, players);
             if (playerscount >= players_count_in_single_game)
             {
                 while (PlayersOfNewRounde(players_of_this_rounde, playerscount, players_count_in_single_game)) {
                     for (int i = 0; i < players_count_in_single_game; i++) std::cout << players_of_this_rounde[i] << " ";
                     std::cout << "\n";
-                    score = Simulator(game_steps_number, game_type, players_of_this_rounde[0], players_of_this_rounde[1], players_of_this_rounde[2], score, roundschoises, players);
+                    score = Simulator(game_steps_number, game_type, players_of_this_rounde[0], players_of_this_rounde[1], players_of_this_rounde[2], score, roundschoices, players);
                     //score.MakeItZero();
                 }
             }
@@ -143,11 +173,11 @@ int main(int argc, char* argv[])
             return 0;
         }
         else if ((game_type == 1) || (game_type == 2)) {//ну и без 
-            std::cout << "do something";
-            score = Simulator(game_steps_number, game_type, 0, 1, 2, score, roundschoises, players);
+            std::cout << "do something\n";
+            score = Simulator(game_steps_number, game_type, 0, 1, 2, score, roundschoices, players);
             score.ShowBestGlobalScore();
             return 0;
         }
         return -1;
-    }///////
+    }
 }
