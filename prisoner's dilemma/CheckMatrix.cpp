d11d2fd4d53ce887854df6ba3e963217378a69ce
evilraptor <43874288+evilraptor@users.noreply.github.com>
2023-01-25 18:37:48 +0700

    Update CheckMatrix.cpp

--- prisoner's dilemma/CheckMatrix.cpp
+++ prisoner's dilemma/CheckMatrix.cpp
@@ -1,9 +1,9 @@
 #include "DilemmaHeader.h"
 
 bool CheckMatrix(int pay_off_matrix[8][6]) {
-	for (int i = 0;i < 7;i++) {
+	for (int i = 0; i < 7; i++) {
 		int tmp1[6] = { 0 };
-		for (int j = 0;j < 6;j++) {
+		for (int j = 0; j < 6; j++) {
 			if (pay_off_matrix[i][j] == 'D') {
 				pay_off_matrix[i][j] = 1;
 				tmp1[j] = 1;
@@ -17,9 +17,9 @@ bool CheckMatrix(int pay_off_matrix[8][6]) {
 		int tmp12 = tmp1[0] * 100 + tmp1[1] * 10 + tmp1[2];
 		//посчитали первую строку
 
-		for (int j = 1;j < 8;j++) {
+		for (int j = 1; j < 8; j++) {
 			int tmp2[6] = { 0 };
-			for (int j2 = 0;j2 < 6;j2++) {
+			for (int j2 = 0; j2 < 6; j2++) {
 				if (pay_off_matrix[i + 1][j2] == 'D') {
 					pay_off_matrix[i + 1][j2] = 1;
 					tmp2[j2] = 1;
@@ -36,10 +36,10 @@ bool CheckMatrix(int pay_off_matrix[8][6]) {
 
 			if (tmp22 < tmp12) {
 				int buffer[6];
-				for (int k = 1;k < 6;k++) {
+				for (int k = 1; k < 6; k++) {
 					buffer[k] = pay_off_matrix[j][k];
 				}
-				for (int k = 1;k < 6;k++) {
+				for (int k = 1; k < 6; k++) {
 					tmp2[k] = pay_off_matrix[i][k];
 					pay_off_matrix[i][k] = buffer[k];
 				}
